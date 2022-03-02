b017d426345e379a102216d34ed3adf572124021
jean-goua <jean-goua@hotmail.fr>
2022-03-02 20:02:06 +0100

    new component, new class

---
+++ src/app/face-snap/face-snap.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FaceSnap } from '../models/face-snap.model';
+
+@Component({
+  selector: 'app-face-snap',
+  templateUrl: './face-snap.component.html',
+  styleUrls: ['./face-snap.component.scss'],
+})
+export class FaceSnapComponent implements OnInit {
+
+  @Input() faceSnap: FaceSnap;
+  buttonText: string;
+
+  ngOnInit(): void {
+    this.buttonText = 'oh Snap!';
+  }
+
+  onSnap() {
+    if (this.buttonText === 'oh Snap!') {
+      this.faceSnap.snaps++;
+      this.buttonText = 'Oups, unSnap!';
+    } else {
+      this.faceSnap.snaps--;
+      this.buttonText = 'oh Snap!';
+    }
+  }
+}
